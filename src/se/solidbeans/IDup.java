d496bf248ad1383a680115d823dba6386159848a
jorbod <jorgen.boderus@solidbeans.com>
2018-09-21 00:06:00 +0200

    Fungerar fungerar men blir outofmemory på stor fil

--- src/se/solidbeans/IDup.java
+++ src/se/solidbeans/IDup.java
@@ -1,11 +1,13 @@
 package se.solidbeans;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
@@ -13,7 +15,7 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 public class IDup extends SimpleFileVisitor<Path> {
 
     private Map<Path, Long> files = new HashMap<>();
-    private Map<Path, Set<Path>> duplicates = new HashMap<>();
+    private Map<Path, Set<Path>> duplicates = new ConcurrentHashMap<>();
 
     public static void main(String[] args) throws IOException {
         IDup iDup = new IDup();
@@ -21,6 +23,10 @@ public class IDup extends SimpleFileVisitor<Path> {
         Files.walkFileTree(startingDir, iDup);
         iDup.findDuplicates();
         iDup.print();
+    }
+
+    private void print() {
+        System.out.println("Antal duplikat: "+duplicates.size()+". Antal filer: "+files.size());
     }
 
     private void findDuplicates() {
@@ -33,25 +39,24 @@ public class IDup extends SimpleFileVisitor<Path> {
 
     }
 
-    private void findDuplicates(Path currentFileKey, Map<Path, byte[]> dupCandidates) {
+    private void findDuplicates(Path currentFileKey, Map<Path, String> dupCandidates) {
         Set<Path> dup = dupCandidates.entrySet().stream()
-                .filter(set -> set.equals(calcChecksum(currentFileKey)))
+                .filter(set -> set.getValue().equals(calcChecksum(currentFileKey)))
                 .map(set -> set.getKey())
                 .collect(Collectors.toSet());
         if (!dup.isEmpty()) {
             this.duplicates.put(currentFileKey, dup);
+            System.out.println();
+            System.out.println(currentFileKey.toString());
+            dup.forEach(x -> System.out.println("       "+x));
         }
     }
 
-    private void print() {
-        duplicates.forEach((path, map) -> System.out.println(path.toString() + map));
-    }
-
-    private Map<Path, byte[]> getDupCandidates(Map.Entry<Path, Long> entry) {
-        Map<Path, byte[]> candiates = new HashMap<>();
+    private Map<Path, String> getDupCandidates(Map.Entry<Path, Long> entry) {
+        Map<Path, String> candiates = new HashMap<>();
         files.entrySet().parallelStream()
                 .filter(candidate -> isNewDuplicate(candidate, entry))
-                .forEach(candidate -> candiates.put(entry.getKey(), calcChecksum(candidate.getKey())));
+                .forEach(candidate -> candiates.put(candidate.getKey(), calcChecksum(candidate.getKey())));
         return candiates;
     }
 
@@ -65,23 +70,29 @@ public class IDup extends SimpleFileVisitor<Path> {
                 duplicates.values().stream().anyMatch(dupSet -> dupSet.contains(set.getKey()));
     }
 
-
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         super.visitFile(file, attrs);
-        byte[] checksum = new byte[0];
-        files.put(file, attrs.size());
+        if (prededate(file)) {
+            files.put(file, attrs.size());
+        }
         return CONTINUE;
     }
 
-    private byte[] calcChecksum(Path file)  {
+    private boolean prededate(Path file) {
+        if (file==null )return false;
+        return !file.toString().contains("iPhoto-bibliotek");
+    }
+
+    private String calcChecksum(Path file)  {
         MessageDigest md = null;
         try {
-            md = MessageDigest.getInstance("MD5");
+            md = MessageDigest.getInstance("SHA1");
             md.update(Files.readAllBytes(file));
         } catch (NoSuchAlgorithmException | IOException e) {
             e.printStackTrace();
         }
-        return md.digest();
+        return DatatypeConverter
+                .printHexBinary(md.digest());
     }
 }
